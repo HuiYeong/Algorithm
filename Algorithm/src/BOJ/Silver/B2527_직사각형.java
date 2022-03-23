318b259b35254af95f3af2b240fdb4262f27d648
HuiYeong <dbgmldud8313@naver.com>
2022-03-23 17:00:19 +0900

    0323

--- Algorithm/src/BOJ/Silver/B2527_직사각형.java
+++ Algorithm/src/BOJ/Silver/B2527_직사각형.java
@@ -5,37 +5,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+ * <수학, 기하학 문제 >
+ * 
+ * 겹칠 때의 조건이 가장 많으니 점일 떄, 분리 되었을 때, 선분일 때로 나눠서 조건처리 후 겹칠 때는 마지막에 else로 처리
+ * 점 - 꼭짓점이 같다.
+ * 분리 - 끝 점끼리 겹치지 않는다.
+ * 선분 - 한 면이 맞닿는다.
+ */
 public class B2527_직사각형 {
+	static int x1, y1, p1, q1, x2, y2, p2, q2;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		for (int i=0;i<4;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int p1 = Integer.parseInt(st.nextToken());
-			int q1 = Integer.parseInt(st.nextToken());
+			x1 = Integer.parseInt(st.nextToken());
+			y1 = Integer.parseInt(st.nextToken());
+			p1 = Integer.parseInt(st.nextToken());
+			q1 = Integer.parseInt(st.nextToken());
 
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			int p2 = Integer.parseInt(st.nextToken());
-			int q2 = Integer.parseInt(st.nextToken());
+			x2 = Integer.parseInt(st.nextToken());
+			y2 = Integer.parseInt(st.nextToken());
+			p2 = Integer.parseInt(st.nextToken());
+			q2 = Integer.parseInt(st.nextToken());
 			
-			sb.append(check(x1, y1, p1, q1, x2, y2, p2, q2) + "\n");
+			sb.append(check() + "\n");
 		}
 		System.out.println(sb);
 	}
 	
-	private static String check(int x1, int y1, int p1, int q1, int x2, int y2, int p2, int q2) {
-        if ((p1 == x2 && q1 == y2) || (x1 == p2 && q1 == y2) || (p1 == x2 && y1 == q2) || (x1 == p2 && y1 == q2)) {
-            return "c";
-        } else if ((p1 == x2 && q1 != y2) || (x1 == p2 && q1 != y2) || (p1 != x2 && y1 == q2) || (x1 != p2 && y1 == q2)) {
-            return "b";
-        } else if (p1 < x2 || p2 < x1 || q1 < y2 || q2 < y1) {
-            return "d";
-        } else {
-            return "a";
-        }
+	private static String check() {
+		// 점일 때
+		if((p1==x2 && q1==y2) || (x1==p2 && q1==y2) || (x1==p2 && y1==q2) || (p1==x2 && y1==q2)) return "c";
+		// 완전 분리 되었을 때
+		else if(q1<y2 || p1<x2 || q2<y1 || p2<x1) return "d";
+		// 선분일 때
+		else if(p1==x2 || q1==y2 || x1==p2 || y1==q2) return "b";
+		// 겹칠 때 (경우의 수가 많으니 아예 조건 빼주기
+		else return "a";
     }
 }
